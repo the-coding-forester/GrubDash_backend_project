55456b574f494fdcce2bcae76a1557e14b84f7bc
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-24 15:18:12 -0400

    Router correction and create diverTo validation complete

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -5,6 +5,19 @@ const nextId = require("../utils/nextId");
 
 //VALIDATION
 
+//check if the deliverTo property is missing
+const bodyHasDeliverTo = (req, res, next) => {
+  const { data: { deliverTo } = {} } = req.body;
+
+  if (deliverTo) {
+    req.deliverTo = deliverTo;
+    return next();
+  }
+  next({
+    status: 400,
+    message: 'Order must have a deliverTo property.'
+  })
+}
 
 //HANDLER MIDDLEWARE
 
@@ -13,6 +26,21 @@ const list = (req, res) => {
   res.status(200).json({ data: orders })
 }
 
+//POST /orders
+const create = (req, res) => {
+  const newOrder = {
+    id: nextId(),
+    deliverTo: req.deliverTo,
+    mobileNumber: req.mobileNumber,
+    status: req.status,
+    dishes: req.dishes,
+  };
+
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+
 module.exports = {
   list,
+  create: [bodyHasDeliverTo, create],
 }
