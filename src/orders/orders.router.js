f3bcfa567a1da326c2629f60c169106d78fcc074
Stephanie Moore <smoore_forestry@outlook.com>
2021-06-24 15:08:58 -0400

    read method and route for orders successful

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
+const controller = require('./orders.controller');
+const methodNotAllowed = require('../errors/methodNotAllowed');
 
-// TODO: Implement the /orders routes needed to make the tests pass
+router
+  .route('/')
+  .get(controller.list)
+  .all(methodNotAllowed);
 
 module.exports = router;
